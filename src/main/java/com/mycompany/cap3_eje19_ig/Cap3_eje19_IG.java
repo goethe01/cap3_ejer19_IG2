c73bbfc8c00822d3b3f67f6eb1cb0e6941adc6d5
goethe01 <124703630+goethe01@users.noreply.github.com>
2023-04-16 19:46:47 -0500

    Update Cap3_eje19_IG.java

--- src/main/java/com/mycompany/cap3_eje19_ig/Cap3_eje19_IG.java
+++ src/main/java/com/mycompany/cap3_eje19_ig/Cap3_eje19_IG.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.cap3_eje19_ig;
 
-/**
- *
- * @author eduar
- */
 public class Cap3_eje19_IG {
 
     public static void main(String[] args) {
